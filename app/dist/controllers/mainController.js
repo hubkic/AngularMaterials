8df535576e24c0aefeb5ed61038a9bc4fe26fb80
Hubert Kicinski <hubert.kicinski@version1.com>
2016-03-01 18:37:03 +0000

    first commit

---
+++ app/dist/controllers/mainController.js
@@ -0,0 +1,15 @@
+/// <reference path="../_all.ts" />
+var ContactManagerApp;
+(function (ContactManagerApp) {
+    var MainController = (function () {
+        function MainController() {
+            this.message = "Hello from dklfj controller";
+        }
+        MainController.prototype.contructor = function () {
+        };
+        MainController.$inject = [];
+        return MainController;
+    }());
+    ContactManagerApp.MainController = MainController;
+})(ContactManagerApp || (ContactManagerApp = {}));
+//# sourceMappingURL=mainController.js.map
---
+++ app/src/controllers/mainController.ts
@@ -0,0 +1,11 @@
+/// <reference path="../_all.ts" />
+
+module ContactManagerApp{
+    export class MainController{
+        static $inject =[];
+        
+        contructor(){
+        }
+        message: string ="Hello from dklfj controller";
+    }
+}
